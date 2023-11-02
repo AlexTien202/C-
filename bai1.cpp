56e6d2c2561f15453a520875fe07b1b0b45ef7a2
ALEXTIEN202 <105052627+AlexTien202@users.noreply.github.com>
2023-11-02 21:55:23 +0700

    Update bai1.cpp

--- bai1.cpp
+++ bai1.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
-    cout << "Hello"; 
+    int n;
+    cout << "Nhap n: ";
+    cin >> n;
+
+    int s = 0;
+    int i = 1;
+    while(i <= n) {
+        s += i;
+        i++;
+    }
+
+    cout << "Kq: S(" << n << ") = " << s;
+    return 0;
 }
